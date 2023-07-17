cb682ee4f9b7e3032e274a9414dda390af216862
JAML <rectorretro@gmail.com>
2023-07-17 14:28:00 -0600

    Flex use, dimanic Freya

--- js/spotify.js
+++ js/spotify.js
@@ -3,14 +3,15 @@
 	console.log(coco);
 }
 saludar();*/
-
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
   const element = document.getElementById('embed-iframe');
   const options = {
-  	  width: '20%',
+  	  width: '100%',
   	  height: '100',
       uri: 'https://open.spotify.com/artist/4gOc8TsQed9eqnqJct2c5v?si=3vJANOKHQci0OjoTVJRsrw'
     };
   const callback = (EmbedController) => {};
   IFrameAPI.createController(element, options, callback);
 };																																									
+
+
